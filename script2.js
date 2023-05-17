5101c6365db622ac37a76434b02f2d34de5b9d57
Rafael Solano <rafaelsolanol007@gmail.com>
2023-05-17 16:53:06 -0500

    Se agregaron los botones e imagenes

---
+++ script2.js
@@ -0,0 +1,28 @@
+
+popupWhatsApp=()=>{
+    
+    //variables
+    let btnclosepopup=document.querySelector('.closePopup');
+    let openpopup=document.querySelector('.whatsapp-button');
+    let popup=document.querySelector('.popup-whatsapp');
+    let sendbtn=document.getElementById('send-btn');
+
+    
+    //evento cerrar poppu
+    btnclosepopup.addEventListener('click',()=>{
+        popup.classList.toggle('is-active-whatsapp-popup')
+    });
+    openpopup.addEventListener("click", ()=>{
+        popup.classList.toggle('is-active-whatsapp-popup')
+    popup.style.animation='fadeIn .6s 0.0s both'
+    });
+    sendbtn.addEventListener('click',()=>{
+        let msg=document.getElementById('whats-in').value;
+        let relmsg=msg.replace(/ /g,'%20');
+        window.open(`https://wa.me/+573133189354?text=${relmsg}_blank`);
+    });
+    setTimeout(() =>{
+        popup.classList.toggle('is-active-whatsapp-popup');
+    }, 3000);
+}
+    popupWhatsApp();
